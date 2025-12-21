38e85721e4ff9f901714406af2a7de7820adf95f
Seriki yakub <sandraisom00@gmail.com>
2025-12-21 16:51:19 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,3 +6,9 @@ git remote add origin https://github.com/username/sctm.git
 git push -u origin main
 
 echo "sctm_code_here" > sctm_code.py
+
+cd /usr/src/redhat/SOURCES
+wget http://www.acme.com/software/mini_httpd/mini_httpd-1.30.tar.gz
+rpm -ta mini_httpd-1.30.tar.gz
+rpm -i /usr/src/redhat/RPMS/i386/mini_httpd-1.30-1.i386.rpm
+    
