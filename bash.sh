3808951e8e4d49ebd2e200d564a81ccf081fceb1
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-06 20:08:34 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,7 +2,7 @@ git init                # initialize repo
 git add my_sctm_code.py # stage file
 git commit -m "Add SCTM code"
 git branch -M main
-git remote add origin https://github.com/username/sctm.git
+git remote add origin https://github.com/web4application/sctm.git
 git push -u origin main
 
 echo "sctm_code_here" > sctm_code.py
