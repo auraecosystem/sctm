1d7f351f68888f2ecfd1e507f108be7119a35569
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-15 02:00:57 +0100

    Update and rename XML.js to xslt/node.js

--- xslt/node.js
+++ xslt/node.js
@@ -3,7 +3,7 @@ const xmlRef = document.implementation.createDocument("", "", null);
 
 // We want to move a part of the DOM from an HTML document to an XML document.
 // importNode is used to clone the nodes we want to process via XSLT - true makes it do a deep clone
-const myNode = document.getElementById("example");
+const myNode = document.getElementById("xlsl");
 const clonedNode = xmlRef.importNode(myNode, true);
 
 // Add the cloned DOM into the XML document
